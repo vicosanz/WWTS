5d7674a5f9056529f6c4454a7462e85416e550dc
Victor Sánchez <vicosanz@gmail.com>
2021-03-19 22:04:56 -0500

    Automate push Github release files

--- Siembra/createnuget.ps1
+++ Siembra/createnuget.ps1
@@ -2,8 +2,14 @@ $project = "Siembra"
 $dll = ".\WWTS.$($project)\WWTS.$($project).Modulo\bin\Release\$($project).exe"
 $fulldll = Resolve-Path $dll
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($fulldll).FileVersion
-$nugetpack = ".\nuget.exe pack"
-Invoke-Expression "$($nugetpack) $($project).nuspec -Version $($version) -properties project=$($project)"
+$nugetpack = ".\nuget.exe pack $($project).nuspec -Version $($version) -properties project=$($project)"
+Write-Output "$($nugetpack)"
+Invoke-Expression "$($nugetpack)"
 
-$squirrelreleasify = "Squirrel -releasify"
-Invoke-Expression "$($squirrelreleasify) .\$($project).$($version).nupkg"
+$squirrelreleasify = "Squirrel -releasify .\$($project).$($version).nupkg"
+Write-Output "$($squirrelreleasifyh)"
+Invoke-Expression "$($squirrelreleasify)"
+
+$push = "C:\Projects\GitHubPushRelease\GitHubPushRelease\bin\Release\net5.0\GitHubPushRelease.exe -u=vicosanz -r=$($project)installer -f='.\Releases'"
+Write-Output "$($push)"
+Invoke-Expression "$($push)"
